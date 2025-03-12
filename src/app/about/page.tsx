d79e7ba6a46ec50194effc430eaa60f67dbf2eaa
Dawit Diriba <dawitccnt@gmail.com>
2025-03-12 13:15:54 +0300

    fix imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,20 @@
-import AboutDetail from '@/Components/About/AboutDetail'
-import AboutTimeline from '@/Components/About/AboutTimeline'
-import AboutUs from '@/Components/About/AboutUs'
-import Team from '@/Components/About/Team'
-import Vision from '@/Components/About/Vision'
-import React from 'react'
+import AboutDetail from "@/components/About/AboutDetail";
+import AboutTimeline from "@/components/About/AboutTimeline";
+import AboutUs from "@/components/About/AboutUs";
+import Team from "@/components/About/Team";
+import Vision from "@/components/About/Vision";
+import React from "react";
 
 const about = () => {
   return (
-    <div className='overflow-hidden bg-bgPrimary'>
-        <AboutUs />
-        <AboutDetail />
-        <Vision />
-        <Team />
-        <AboutTimeline />
+    <div className="overflow-hidden bg-bgPrimary">
+      <AboutUs />
+      <AboutDetail />
+      <Vision />
+      <Team />
+      <AboutTimeline />
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default about;
