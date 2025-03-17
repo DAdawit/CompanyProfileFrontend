ab59dc38496a8f9351e7c7623b4b74d9b5eee93b
IdentityG <babayesara1@gmail.com>
2025-03-17 19:59:42 +0300

    services change

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,16 +1,16 @@
-import Hero from "@/components/Service/Hero";
-import OurService from "@/components/Service/OurService";
-import Stats from "@/components/Service/Stats";
-import React from "react";
+import Hero from '@/Components/Service/Hero'
+import OurService from '@/Components/Service/OurService'
+import Stats from '@/Components/Service/Stats'
+import React from 'react'
 
 const service = () => {
   return (
-    <div className="overflow-hidden bg-primary-50">
-      <Hero />
-      <OurService />
-      <Stats />
+    <div className='overflow-hidden bg-primary-500'>
+        <Hero />
+        <OurService />
+        <Stats />
     </div>
-  );
-};
+  )
+}
 
 export default service;
