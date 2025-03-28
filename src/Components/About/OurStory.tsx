bca48c885aec7853f759c91b6d1657d4160665fe
Dawit Diriba <dawitccnt@gmail.com>
2025-03-28 17:07:52 +0300

    about section fix

--- src/Components/About/OurStory.tsx
+++ src/Components/About/OurStory.tsx
@@ -11,13 +11,15 @@ const DynamicTitleAndDescription = ({
   history,
 }: PropType) => {
   return (
-    <div className="grid grid-cols-12 justify-center text-center mb-12">
+    <div className="grid grid-cols-12 justify-center mb-12">
       <div className="col-span-12 md:col-span-8 md:col-start-3">
-        <h2 className="text-4xl leading-snug md:text-5xl md:leading-snug font-bold mb-6">
+        <h2 className="text-4xl leading-snug md:text-5xl md:leading-snug font-bold mb-6 text-center">
           {title}
         </h2>
-        <p className="text-xl opacity-80 mb-4">{description}</p>
-        <p className="text-xl opacity-80 mb-4">{history}</p>
+        <p className="text-lg md:text-xl opacity-80 mb-4 px-5 text-center ">
+          {description}
+        </p>
+        <p className="text-xl opacity-80 mb-2">{history}</p>
       </div>
     </div>
   );
